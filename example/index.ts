6b0a8465aecc529595fef4627ef611f37b910ebf
wakkihaya <wakkihaya@gmail.com>
2024-12-12 23:12:06 +0900

    fix: revert example

--- example/index.ts
+++ example/index.ts
@@ -5,31 +5,27 @@ async function main() {
   const clientSecret = "your_client_secret";
 
   // AUTH ====
-  // const authUrl = authTickTick({
-  //   clientId,
-  //   scopes: ["tasks:write"],
-  //   redirectUri: "http://localhost:3000/auth/callback",
-  // });
+  const authUrl = authTickTick({
+    clientId,
+    scopes: ["tasks:write"],
+    redirectUri: "http://localhost:3000/auth/callback",
+  });
 
-  // const tokens = await exchangeToken({
-  //   code: "your_code",
-  //   clientId,
-  //   clientSecret,
-  //   scopes: ["tasks:write"],
-  //   redirectUri: "http://localhost:3000/auth/callback",
-  // });
+  const tokens = await exchangeToken({
+    code: "your_code",
+    clientId,
+    clientSecret,
+    scopes: ["tasks:write"],
+    redirectUri: "http://localhost:3000/auth/callback",
+  });
 
   // PROJECT ====
-  const ticktick = new TickTickNode("fda35be0-d05a-4951-bf44-64343a5fc657");
+  const ticktick = new TickTickNode("your_access_token");
   const project = await ticktick.fetchProjects();
 
-  console.log(project);
+  // USER ===
+  const user = await ticktick.fetchUserInfo();
 
-  // // USER ===
-  // const user = await ticktick.fetchUserInfo();
-
-  // // TASK ===
-  // const tasks = await ticktick.fetchTaskById("project_id", "task_id");
+  // TASK ===
+  const tasks = await ticktick.fetchTaskById("project_id", "task_id");
 }
-
-main();
